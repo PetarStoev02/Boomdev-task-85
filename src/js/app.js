275fd97805d10c4073d904095a18bd18743a2034
Петър Стоев <petarstoev0505@gmail.com>
2022-12-12 16:32:12 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
-  
+
   fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10`)
     .then((resp) => resp.json())
     .then((data) => {
       let arr = data.results;
-     
-      arr.forEach(element => {
+
+      arr.forEach((element) => {
         let li = document.createElement("li");
         li.innerHTML = ` ${element.name}`;
         ul.appendChild(li);
